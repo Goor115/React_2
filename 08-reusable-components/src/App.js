4a661ba14253da89c42023924fe0cd34689f8d4f
Igor <lapayigor@gmail.com>
2023-07-13 10:44:53 +0300

    Done-119

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import MyComponent from './components/Mycomponent'
+import AtherComponent from './components/AtherComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+      <AtherComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ 08-reusable-components/src/components/AtherComponent.js
@@ -0,0 +1,12 @@
+function AtherComponent() {
+  return (
+    <div>
+      <p>
+        Loremkdcjnbsvkjbsdkvbskdvv b k b bd bsd b vbdsvb sdbv sdhjbhj
+        dbsbsdbsdbvjsdb vlhsdbvsdbvbs dkl
+      </p>
+    </div>
+  )
+}
+
+export default AtherComponent
