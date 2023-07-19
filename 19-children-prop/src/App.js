fb0511b9b8994d0cc94602954fc19cbe7f934437
Igor <lapayigor@gmail.com>
2023-07-19 10:28:12 +0300

    done_20

---
+++ 19-children-prop/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Wrapper from './components/Wrapper'
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper color="lightblue">
+        <h1>Text inside of the Wrapper</h1>
+      </Wrapper>
+    </div>
+  )
+}
+
+export default App
