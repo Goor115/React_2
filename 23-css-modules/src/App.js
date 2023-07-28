fea074c6e0a96bba139d0917a800ee64504b21ed
Igor <lapayigor@gmail.com>
2023-07-28 10:30:50 +0300

    done_23

---
+++ 23-css-modules/src/App.js
@@ -0,0 +1,14 @@
+import Info from './components/Info'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+      <h1>App compomemt heading</h1>
+      <button className="my-button">Click me</button>
+    </div>
+  )
+}
+
+export default App
